d3861b41d6b35c254a3ed0c5a61a32aacbce4ad9
eliezer-web <evj.97160@gmail.com>
2021-05-19 14:17:00 +0200

    start nav bar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
+import HomePage from '../src/pages/Homepage';
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <Router>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+      </Switch>
+    </Router>
+  </div>;
 }
 
 export default App;
