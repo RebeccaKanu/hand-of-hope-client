3e616e86f7fc037cb69e17d2d4f09b27b32124f3
nawalpro <nawal.maizate@gmail.com>
2021-05-19 14:25:36 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App"><h1>Bonjour</h1></div>;
 }
 
 export default App;
