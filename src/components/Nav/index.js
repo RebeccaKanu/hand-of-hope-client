4eb3b453a77b14c8f1f7524788f0d4d323efaa06
RebeccaKanu <kanurebecca0@gmail.com>
2021-05-24 18:56:51 +0200

    header style almost done just lack contact animation

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -21,9 +21,24 @@ class index extends Component {
       // transition: "",
       // height: "",
       // zIndex: 10,
-      // contactAdress: "",
     };
   }
+
+  showAdress = () => {
+    this.setState({
+      contactAdressPart1: "5 allée de la Houdiarde",
+      contactAdressPart2: "VEMARS 95470",
+    });
+  };
+
+  hideAdress = () => {
+    this.setState({
+      contactAdressPart1: "",
+      contactAdressPart2: "",
+      contactHeight: 0,
+      contactwidth: 0,
+    });
+  };
 
   //on remplie le menu qui était initialement vide pour que lorsque l'on clique les éléments du menu s'affiche
   showMenu = () => {
@@ -33,23 +48,16 @@ class index extends Component {
       don: "Faire un don",
       nous: "Qui sommes nous ?",
       sengager: "S'engager",
-      // contactAdress: "Bonjour"
+      // style
+      width: "80vw", //80vw
+      backgroundColor: "white",
+      opacity: 0.6,
+      height: "97vh",
+      transformTop: "rotate(45deg) translate(3px, 4px)",
+      transformBottom: "rotate(-45deg) translate(3px, -4px)",
+      background: "transparent",
     });
   };
-
-  // componentDidUpdate () {
-
-  //   showAdress = ()=> {
-
-  //     this.setState({
-
-  //           contactAdress: "Bonjour",
-
-  //     })
-  //     console.log('yoooooo!!!');
-  //   }
-
-  // }
 
   //on revide le menu pour qu'au clique pour le femer le menu disparaisse
   hideMenu = () => {
@@ -59,16 +67,26 @@ class index extends Component {
       don: "",
       nous: "",
       sengager: "",
+      // opacity: 0.2,
+      // transition: "all 0.1s ease-out",
+      height: 0,
+      //ce qui dans la fonction compteur
       width: 0,
       backgroundColor: "",
-      opacity: 0.2,
-      transition: "all 0.1s ease-out",
-      height: 0,
+      opacity: 0,
+      transition: "all 0.8s ease-out",
+      transformTop: "",
+      transformBottom: "",
+      background: "black",
+      color: "black",
+      // backgroundColor: "transparent",//?
+      boxShadow: "10px 5px 5px black",
+      contactAdress: "",
     });
   };
 
   //on créer un compteur pour qu'au premier(0) clique le menu s'affiche au second(1) il disparaissent au troisième(2) il apparaissent etc...
-  compteur = () => {
+  burgerMenuCompteur = () => {
     this.setState({
       compteur: this.state.compteur + this.state.n,
     });
@@ -77,32 +95,27 @@ class index extends Component {
       //au deuxième clique cacher le menu => deuxième clique = compteur à 2
       //autrement dit si le compteur = chiffre impair cacher le menu (soit 1 pour le deuxième clique, 3 pour le troisième clique etc...) sachant que le deuxième clique sera forcément pour fermer le menu
       this.hideMenu();
-      this.setState({
-        width: 0,
-        backgroundColor: "",
-        opacity: 0,
-        transition: "all 0.8s ease-out",
-        transformTop: "",
-        transformBottom: "",
-        background: "black",
-        color: "black",
-        // backgroundColor: "transparent",//????
-        boxShadow: "10px 5px 5px black",
-        // contactAdress: "",
-      });
+      this.hideAdress();
     } else {
       //au premier clique afficher le menu => premier clique = compteur à 1
       //autrement dit si compteur = chiffre pair afficher le menu
       this.showMenu();
-      this.setState({
-        width: "80vw", //80vw
-        backgroundColor: "white",
-        opacity: 0.6,
-        height: "97vh",
-        transformTop: "rotate(45deg) translate(3px, 4px)",
-        transformBottom: "rotate(-45deg) translate(3px, -4px)",
-        background: "transparent",
-      });
+    }
+  };
+  adressCompteur = () => {
+    this.setState({
+      compteur: this.state.compteur + this.state.n,
+    });
+
+    if (this.state.compteur % 2) {
+      //au deuxième clique cacher le menu => deuxième clique = compteur à 2
+      //autrement dit si le compteur = chiffre impair cacher le menu (soit 1 pour le deuxième clique, 3 pour le troisième clique etc...) sachant que le deuxième clique sera forcément pour fermer le menu
+      this.showAdress();
+    } else {
+      //au premier clique afficher le menu => premier clique = compteur à 1
+      //autrement dit si compteur = chiffre pair afficher le menu
+
+      this.hideAdress();
     }
   };
 
@@ -112,8 +125,6 @@ class index extends Component {
         className="BurgerLink"
         style={{
           backgroundColor: this.state.backgroundColor,
-
-          // transition: this.state.transition,
         }}
       >
         {/*au clique du menu burger soit le menu s'affiche (if) soit il
@@ -123,15 +134,9 @@ class index extends Component {
           className="Links"
           style={{ width: this.state.width, height: this.state.height }}
         >
+          {/* <div> */}
           {/*changement dynamique des va this.hideMenu();leurs des liens de nav en alternant entre string vide et valeur exemple au clique on passe de lien invisible c'est à dire "" à des liens qui deviennent visible comme "contact"*/}
-          <li className="contacts">
-            <a href="/contact" onMouseOver={this.compteur}>
-              {this.state.contact}
-            </a>
-          </li>
-          <p>{this.state.contactAdress}</p>
-
-          {/* <li>
+          <li>
             <a href="/actualite">{this.state.actualite}</a>
           </li>
           <li>
@@ -142,9 +147,24 @@ class index extends Component {
           </li>
           <li>
             <a href="/sengager">{this.state.sengager}</a>
-          </li> */}
+          </li>
+
+          <div className="contactContainer">
+            <li className="contacts">
+              <a href="#" onClick={this.adressCompteur}>
+                {this.state.contact}
+              </a>
+              <div className="contactPart1">
+                <p>{this.state.contactAdressPart1}</p>
+              </div>
+              <div className="contactPart2">
+                <p>{this.state.contactAdressPart2}</p>
+              </div>
+            </li>
+          </div>
+          {/* </div> */}
         </ul>
-        <div className="burgerMenu" onClick={this.compteur}>
+        <div className="burgerMenu" onClick={this.burgerMenuCompteur}>
           <div style={{ transform: this.state.transformTop }}></div>
           <div style={{ background: this.state.background }}></div>
           <div style={{ transform: this.state.transformBottom }}></div>
