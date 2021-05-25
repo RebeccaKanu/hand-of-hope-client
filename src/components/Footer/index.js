bd15eef0d4b5d3a90c4aea2ff538c68ff8868756
nawalpro <nawal.maizate@gmail.com>
2021-05-25 10:12:57 +0200

    footer wip

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
-import logo from "../../assets/images/logo-hoh.png";
-import Nav from "../Nav";
-import Contact from "../Contact";
 import "../../assets/stylesheets/footer.scss";
+import {
+  BrowserRouter as Router,
+  Link,
+} from "react-router-dom";
+import logo from "../../assets/images/logo-hoh.png";
+import Contact from "../Contact";
 
 export default class index extends Component {
 
@@ -16,32 +19,34 @@ export default class index extends Component {
           </div>
 
           <span></span>
-
-          <div className="copyrightNav">
-            <p>
-              Fait avec amour par NAWAL, JEROME ET REBECCA <span>Tous droits réservés
-              © 2021</span>
-            </p>
-            <ul className="Nav_links">
-          <li>
-            <a href="/contact"></a>
-          </li>
-          <li>
-            <a href="/actualite"></a>
-          </li>
-          <li>
-            <a href="/don"></a>
-          </li>
-          <li>
-            <a href="/nous"></a>
-          </li>
-          <li>
-            <a href="/sengager"></a>
-          </li>
-        </ul>
-          <Contact />
-        </div>
-        <Nav/>
+          <div>
+            <div className="copyrightNav">
+              <p>
+                Fait avec amour par NAWAL, JEROME ET REBECCA <span>Tous droits réservés
+                © 2021</span>
+              </p>
+            </div>
+            <div>
+              <ul className="Nav_links">
+                <li>
+                  <Link to="/contact">Contact</Link>
+                </li>
+                <li>
+                  <Link to="/actualite">Actualitée</Link>
+                </li>
+                <li>
+                  <Link to="/don">Faire un don</Link>
+                </li>
+                <li>
+                  <Link to="/nous">Qui sommes-nous ?</Link>
+                </li>
+                <li>
+                  <Link to="/sengager">S'engager</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <Contact/>
         </footer>
       </>
     );
