b4df5ff5e34649427e95aae7c307a0fbbbafd0eb
eliezer-web <evj.97160@gmail.com>
2021-05-26 16:22:33 +0200

    320px

--- src/components/Actuality/index.js
+++ src/components/Actuality/index.js
@@ -25,60 +25,60 @@ export default class index extends Component {
           <span></span>
         </section>
 
-        <article>
-          <figure>
-            <img className="imgcard" src={mainArticleOne} alt="taylor" width="280px"  height="400px"
-             />
-            <figcaption>CAMPAGNE DE DISTRIBUTION</figcaption>
-            <a href="#">Scolarité</a>
-          </figure>
-          <h3>TITRE DE L'ARTICLE</h3>
-          <p>ÉCRIS PAR MARYSE CONDÉ</p>
-          <p>Article de la campagne de distribution</p>
-          <button className="button1">
-            <Link to="#">Lire la suite &nbsp; &rarr;</Link>
-          </button>
-        </article>
-
-        <article>
-          <figure>
-            <img className="imgcard" src={mainArticleTwo} alt="taylor" width="280px"  height="400px"/>
-            <figcaption className="figcaption2">
-              CAMPAGNE DE DISTRIBUTION
-            </figcaption>
-            <a className="a2" href="#">
-              Enseignement
-            </a>
-          </figure>
-          <div className="box">
+        <div>
+          <article>
+            <figure>
+              <img className="imgcard" src={mainArticleOne} alt="taylor" width="280px"  height="400px"
+               />
+              <figcaption>CAMPAGNE DE DISTRIBUTION</figcaption>
+              <a href="#">Scolarité</a>
+            </figure>
             <h3>TITRE DE L'ARTICLE</h3>
-            <p>ÉCRIS PAR MARYSE CONDÉ</p>
+            <h4>ÉCRIS PAR MARYSE CONDÉ</h4>
             <p>Article de la campagne de distribution</p>
-            <button className="button2">
+            <button className="button1">
               <Link to="#">Lire la suite &nbsp; &rarr;</Link>
             </button>
-          </div>
-        </article>
+          </article>
 
-        <article>
-          <figure>
-            <img className="imgcard" src={mainArticleThree} alt="taylor" width="280px" height="400px"/>
-            <figcaption className="figcaption3">
-              CAMPAGNE DE DISTRIBUTION
-            </figcaption>
-            <a className="a3" href="#">
-              Scolarité
-            </a>
-          </figure>
-          <h3>TITRE DE L'ARTICLE</h3>
-          <p>ÉCRIS PAR MARYSE CONDÉ</p>
-          <p>Article de la campagne de distribution</p>
-          <button className="button3">
-            <Link to="#">
-              Lire la suite &nbsp;<em>&rarr;</em>
-            </Link>
-          </button>
-        </article>
+          <article>
+            <figure>
+              <img className="imgcard" src={mainArticleTwo} alt="taylor" width="280px"  height="400px"/>
+              <figcaption className="figcaption2">
+                CAMPAGNE DE DISTRIBUTION
+              </figcaption>
+              <a className="a2" href="#">
+                Enseignement
+              </a>
+            </figure>
+              <h3>TITRE DE L'ARTICLE</h3>
+              <h4>ÉCRIS PAR MARYSE CONDÉ</h4>
+              <p>Article de la campagne de distribution</p>
+              <button className="button2">
+                <Link to="#">Lire la suite &nbsp; &rarr;</Link>
+              </button>
+          </article>
+
+          <article>
+            <figure>
+              <img className="imgcard" src={mainArticleThree} alt="taylor" width="280px" height="400px"/>
+              <figcaption className="figcaption3">
+                CAMPAGNE DE DISTRIBUTION
+              </figcaption>
+              <a className="a3" href="#">
+                Scolarité
+              </a>
+            </figure>
+            <h3>TITRE DE L'ARTICLE</h3>
+            <h4>ÉCRIS PAR MARYSE CONDÉ</h4>
+            <p>Article de la campagne de distribution</p>
+            <button className="button3">
+              <Link to="#">
+                Lire la suite &nbsp;<em>&rarr;</em>
+              </Link>
+            </button>
+          </article>
+        </div>
       </div>
     );
   }
